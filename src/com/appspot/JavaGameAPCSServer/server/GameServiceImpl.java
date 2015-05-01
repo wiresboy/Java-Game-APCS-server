c0dca89ea8c90015c8428474a897221e64e6c2da
Brandon John <wiresboy@yahoo.com>
2015-04-30 19:21:50 -0500

    Set up a server
    Outline code, still not totally sure what is going on.

---
+++ src/com/appspot/JavaGameAPCSServer/server/GameServiceImpl.java
@@ -0,0 +1,31 @@
+package com.appspot.JavaGameAPCSServer.server;
+
+import com.appspot.JavaGameAPCSServer.client.GreetingService;
+import com.appspot.JavaGameAPCSServer.shared.FieldVerifier;
+import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
+/**
+ * The server-side implementation of the RPC service.
+ */
+@SuppressWarnings("serial")
+public class GameServiceImpl extends RemoteServiceServlet implements
+		GreetingService {
+
+	public String greetServer(String input) throws IllegalArgumentException {
+		// Verify that the input is valid. 
+		if (!FieldVerifier.isValidName(input)) {
+			// If the input is not valid, throw an IllegalArgumentException back to
+			// the client.
+			throw new IllegalArgumentException(
+					"Name must be at least 4 characters long");
+		}
+
+		String serverInfo = getServletContext().getServerInfo();
+		String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+
+		return "Hello, " + input + "!<br><br>I am running " + serverInfo
+				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+	}
+
+
+}

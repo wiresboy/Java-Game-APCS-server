d3ce81b8ab6c71c52f7cab09f613a5deb5ed1a53
Brandon John <wiresboy@yahoo.com>
2015-05-06 11:24:18 -0500

    Starting to add GameStore logic
    Still unsure if we will even be able to use this... Hope we can!!!

--- src/com/appspot/JavaGameAPCSServer/server/GameServiceImpl.java
+++ src/com/appspot/JavaGameAPCSServer/server/GameServiceImpl.java
@@ -5,39 +5,38 @@ import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.Properties;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * The server-side implementation of the RPC service.
- */
 @SuppressWarnings("serial")
 public class GameServiceImpl extends HttpServlet {
 
 	@Override
-	  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-	      throws IOException {
-	    if (req.getParameter("testing") == null) {
-	      resp.setContentType("text/plain");
-	      resp.getWriter().println("Hello, this is a testing servlet. \n\n");
-	      Properties p = System.getProperties();
-	      p.list(resp.getWriter());
-
-	    } else {
-	      UserService userService = UserServiceFactory.getUserService();
-	      User currentUser = userService.getCurrentUser();
-
-	      if (currentUser != null) {
-	        resp.setContentType("text/plain");
-	        resp.getWriter().println("Hello, " + currentUser.getNickname());
-	      } else {
-	        resp.sendRedirect(userService.createLoginURL(req.getRequestURI()));
-	      }
-	    }
-	  }
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		byte b[] = new byte[1024];
+		String input = "";
+		while (req.getInputStream().read(b)>0)
+			input = input + b.toString();
+		req.getMethod();
+		PrintWriter out = resp.getWriter();
+		out.println("TEST11111");
+		out.println("Method: "+req.getMethod());
+		out.print("Cookies: ");
+		for (Cookie c : req.getCookies())
+			out.println(c.getName()+": "+c.getValue());
+		out.println("Remote Address: "+req.getRemoteAddr());
+		out.println("game: "+req.getParameter("game"));
+		out.println("me: "+req.getParameter("me"));
+		out.println("it: "+req.getParameter("it"));
+		out.print(input);
+		
+	}
 
 
 }
